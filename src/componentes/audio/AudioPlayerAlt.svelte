64d6892eaa07cb76a9d9995fbd0cd6e551906c67
josecaos <jazzvoon@gmail.com>
2019-10-06 17:51:26 -0500

    Funcion play integrada

--- src/componentes/audio/AudioPlayerAlt.svelte
+++ src/componentes/audio/AudioPlayerAlt.svelte
@@ -7,30 +7,58 @@
   export let creditoAudio;
   export let derechosAudio;
 
-  let botonPlay;
-  let controlAmp;
-  let barraProgreso;
-  let canal;
+  let botonPlay;//bind
+  let controlAmp;//bind
+  let barraProgreso;//bind
+  let conexion;
   let ampNodo;
-
   // let AudioContext = window.AudioContext || window.webkitAudioContext;
   const contexto = new AudioContext();
   let audioElemento = new Audio();
-  canal = contexto.createMediaElementSource(audioElemento);
+
+  conexion = contexto.createMediaElementSource(audioElemento);
   ampNodo = contexto.createGain();
   audioElemento.src = archivoAudio;
-  audioElemento.type = "audio/ogg";
-  // audioElemento.controls = false;
-  audioElemento.controls = true;
+  // audioElemento.src = "ambiente-interior.ogg";
+  // audioElemento.type = "audio/ogg";
+  audioElemento.controls = false;
   audioElemento.autoplay = false;
   audioElemento.loop = false;
 
-  // let playButton = document.querySelector(".toggle-play");
-  // let volumeControl = document.querySelector("#volumen");
-  // let progressBar = document.querySelector("progress");
+const play = (elemento,conexiones,contexto,audioelement,fileURL,gain,progressbar) => {
+      
+      let prog;
+      // clearInterval(prog)
+      conexion.connect(gain).connect(contexto.destination);
+          
+          if (contexto.state === "suspended") {
+            contexto.resume();
+          }
 
-  console.log(audioElemento.duration);
-  // onMount(() => {});
+          if (elemento.dataset.playing === "false") {
+            // elemento.setAttribute("src", fileURL);
+            audioelement.play();
+            elemento.dataset.playing = "true";
+            // prog = setInterval(() => {
+            //   //sequencia la barra de progreso
+            //   console.log("setInterval ON");
+            //   progress(progressbar, audioelement);
+            // }, 250);
+            // progreso del track
+          } else if (elemento.dataset.playing === "true") {
+            audioelement.pause();
+            elemento.dataset.playing = "false";
+            // clearInterval(prog);
+          }
+
+    }
+
+  onMount(() => {
+   document.addEventListener("click",() => {
+     play(botonPlay,conexion,contexto,audioElemento,archivoAudio,ampNodo,barraProgreso)
+   })
+    
+  });
 </script>
 
 <style>
@@ -46,12 +74,12 @@
     <div class="nombre">
       <h3>Nombre: {nombreAudio}</h3>
     </div>
-    <progress min="0" max="100" value="0" />
+    <progress bind:this={barraProgreso} min="0" max="100" value="0" />
     <br />
-    <input type="range" id="volumen" min="0" max="1" value="1" step="0.01" />
+    <input bind:this={controlAmp} type="range" id="volumen" min="0" max="1" value="1" step="0.01" />
     <br />
     <button
-      on:click={e => console.log('Play')}
+      bind:this={botonPlay}
       class="toggle-play"
       data-playing="false"
       role="switch"
