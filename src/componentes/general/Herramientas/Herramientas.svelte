08d6f7a088b727160a669efd3b0931f7b63a7960
MitziYolotzin <mitziulter@gmail.com>
2019-10-14 02:30:48 -0500

    colocar tooltip para botones de herramientas

--- src/componentes/general/Herramientas/Herramientas.svelte
+++ src/componentes/general/Herramientas/Herramientas.svelte
@@ -25,8 +25,8 @@
     width: 17px;
   }
   .Riesgo:hover,
-  .Riego:focus,
-  .Riego:active {
+  .Riesgo:focus,
+  .Riesgo:active {
     background: rgba(255, 97, 97, 1);
   }
   .Voluta {
@@ -43,13 +43,96 @@
   .Voluta:active {
     background: #465D72;
   }
+
+
+/* TOOLTIPS */
+.tooltipRiesgo .tooltiptext {
+  visibility: hidden;
+  position: absolute;
+  width: 65px;
+  background-color: rgba(255, 97, 97, 1);
+  color: #fff;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+  z-index: 1;
+  opacity: 0;
+  transition: opacity 0.3s;
+  top: 120%;
+  left: 45%;
+  margin-left: -60px;
+  font-size: .75rem;
+}
+
+.tooltipRiesgo .tooltiptext::after {
+  content: "";
+  position: absolute;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent transparent rgba(255, 97, 97, 1) transparent;
+  
+}
+
+.tooltipRiesgo:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
+
+.tooltipVoluta .tooltiptext {
+  visibility: hidden;
+  position: absolute;
+  width: 65px;
+  background-color: #465D72;
+  color: #fff;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+  z-index: 1;
+  opacity: 0;
+  transition: opacity 0.3s;
+  top: 120%;
+  left: 96%;
+  margin-left: -60px;
+  font-size: .75rem;
+}
+
+.tooltipVoluta .tooltiptext::after {
+  content: "";
+  position: absolute;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent transparent #465D72 transparent;
+  
+}
+
+.tooltipVoluta:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
+
+
+
+  
 </style>
 
 <section>
-  <button class="Riesgo">
+<div class="tooltipRiesgo"> 
+  <span class="tooltiptext">Riesgo</span>
+  <button class="Riesgo" >
     <img src={IconoRiesgo} alt="Riesgo de extinción de la lengua" />
   </button>
+</div>
+<div class="tooltipVoluta">
+  <span class="tooltiptext">Aforismos</span>
   <button class="Voluta">
     <img src={IconoVoluta} alt="Texto animado" />
   </button>
+</div>
+
 </section>
