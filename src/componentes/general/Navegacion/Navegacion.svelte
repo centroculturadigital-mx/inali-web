efaa06a4f75a91f2d955fc76d6854d6bed6cbed0
MitziYolotzin <mitziulter@gmail.com>
2019-10-17 20:16:06 -0500

    en componente navegacion ajuste de cambio de posicion de la seccion menu, y se añadio hover en el menu

--- src/componentes/general/Navegacion/Navegacion.svelte
+++ src/componentes/general/Navegacion/Navegacion.svelte
@@ -69,7 +69,8 @@
     height: 100%;
     display: flex;
     align-items: center;
-    justify-content: space-evenly;
+    /* justify-content: space-evenly; */
+    justify-content: flex-end;
     text-transform: uppercase;
 
   }
@@ -127,12 +128,6 @@
   }
 
 /* Hover line */
-  .ListaNavegacion * {
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    -webkit-transition: all 0.35s ease;
-    transition: all 0.35s ease;
-  }
 
   .ListaNavegacion li {
     display: inline-block;
@@ -154,10 +149,11 @@
     opacity: 0;
     left: 15%;
     right: 15%;
-    top: 0;
+    top: 33%;
     bottom: 0;
     border-left: 1px solid #72A6AA;
     border-right: 1px solid #72A6AA;
+    height: 32%;
   }
   
   .ListaNavegacion a:hover:before {
@@ -185,12 +181,12 @@
         glosario
       </a>
     </li>
-    <li>
+    <!-- <li>
       <a
         href="http://138.68.223.192:4000" target="_blank">
         mapa interactivo
       </a>
-    </li>
+    </li> -->
   </ul>
   {:else}
   <div class="InputBusqueda" bind:this={buscadorInput} transition:fade="{{duration: 500 }}">
