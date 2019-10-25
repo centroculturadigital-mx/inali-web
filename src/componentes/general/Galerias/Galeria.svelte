475f364a04d9bf49723245c436a9b06202bb4745
MitziYolotzin <mitziulter@gmail.com>
2019-10-25 01:31:47 -0500

    ajuste 2column  mediaqueries en componente Galeria Fotos, pendiente num columnas en mw 320px

--- src/componentes/general/Galerias/Galeria.svelte
+++ src/componentes/general/Galerias/Galeria.svelte
@@ -50,13 +50,13 @@
 
 @media  (max-width: 575.98px) {
   .Galeria ul {
-    grid-template-columns: 1fr;
+    grid-template-columns: 1fr 1fr;
   }
-  img {
+  /* img {
     max-width: 12rem;
     object-fit: cover;
     border-radius: 3px;
-  }
+  } */
 }
 
 @media (min-width: 576px) and (max-width: 768px)  {
