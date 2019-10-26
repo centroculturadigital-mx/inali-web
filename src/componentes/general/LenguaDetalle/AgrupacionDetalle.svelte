fc547443e21ea884271fd1f87adb5e299d30b37c
ledautopica <vane.multimedia@gmail.com>
2019-10-25 21:36:56 -0500

    ajustes ventana detalle

--- src/componentes/general/LenguaDetalle/AgrupacionDetalle.svelte
+++ src/componentes/general/LenguaDetalle/AgrupacionDetalle.svelte
@@ -49,14 +49,10 @@
       "También llamada algonquina o algonquina-ritwan. \n\nEl nombre de la familia lingüística álgica proviene de la contracción del nombre de una de las dos subfamilias que la integran, la algonquina (alg-), y la adhesión a ésta de una partícula (-ic) que señala pertenencia a algo. Cabe mencionar que, dentro de esta familia, las lenguas algonquinas han tenido, en términos de cobertura geográfica, diversidad interna y número de hablantes, mucha relevancia. \n\nLas lenguas de esta familia se hablan en gran parte de Canadá y en el extremo norte de los Estados Unidos de América, teniendo una concentración muy significativa en la región de los Grandes Lagos. El Kickapoo, que pertenece a la subfamilia algonquina, es el único idioma de esta familia que se habla en México. La llegada de los kickapoo a territorio nacional se dio en el siglo XIX cuando, después de una invasión de los anglosajones a su territorio, le pidieron al gobierno mexicano un espacio para vivir y éste, a cambio, les solicitó que defendieran a los pobladores mexicanos de los ataques de los comanches, muy frecuentes en aquel tiempo. Desde ese momento, los kickapoo son considerados un grupo binacional. En los Estados Unidos de América a los kickapoo de México ."
   };
 
-  
-
-
 
   const RiesgoIcono = `<svg width="25" height="24" viewBox="0 0 25 24" xmlns="http://www.w3.org/2000/svg"><path d="M12.2666 0C5.52183 0 0.0666504 5.36575 0.0666504 12C0.0666504 18.6216 5.52183 24 12.2666 24C19.0115 24 24.4667 18.6342 24.4667 12C24.4538 5.36575 18.9986 0 12.2666 0ZM13.7497 20.1818C13.4015 20.5243 12.9115 20.7019 12.2796 20.7019C11.6347 20.7019 11.1318 20.537 10.7836 20.1945C10.4354 19.8647 10.2548 19.3827 10.2548 18.7738C10.2548 18.1395 10.4225 17.6575 10.7707 17.3404C11.1189 17.0106 11.6218 16.8583 12.2796 16.8583C12.9115 16.8583 13.4144 17.0233 13.7626 17.3531C14.1108 17.6829 14.2914 18.1649 14.2914 18.7738C14.2785 19.37 14.0979 19.8393 13.7497 20.1818ZM13.7626 14.9049H10.7965L9.76475 5.80972C9.68737 4.74419 10.8352 3.85624 12.2796 3.85624C13.7239 3.85624 14.8717 4.74419 14.7943 5.80972L13.7626 14.9049Z"/></svg>`;
 
-
-    const IconoTextiles = "icono.textiles.svg";
+  const IconoTextiles = "icono.textiles.svg";
   const IconoFotos = "icono.fotos.svg";
   const IconoAudios = "icono.audios.svg";
 
@@ -64,6 +60,45 @@
 </script>
 
 <style>
+@media  (max-width: 575.98px) {
+  
+    .DetalleOrigen {
+    /*display: flex;*/
+    justify-content: space-evenly;
+    height: 4.25rem;
+    width: 100%;
+    margin-bottom: 1rem;
+  }
+
+  .DetalleOrigenRiesgo {
+    background-color: rgba(255, 0, 0, 0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    /* max-width: 33%; */
+  }
+
+  .DetalleOrigenFamilia {
+    background-color: transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    /* max-width: 33%; */
+  }
+
+  .DetalleOrigenVariantes {
+    background-color: transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* max-width: 33%; */
+    /*border-left: 1px solid #000;*/
+    width: 100%;
+  }
+ }
+
 .DetalleContenedor {
     display: flex;
     justify-content: flex-start;
@@ -284,45 +319,7 @@
     padding: 0 1rem;
   }
   
-  @media  (max-width: 575.98px) {
   
-    .DetalleOrigen {
-    /*display: flex;*/
-    justify-content: space-evenly;
-    height: 4.25rem;
-    width: 100%;
-    margin-bottom: 1rem;
-  }
-
-  .DetalleOrigenRiesgo {
-    background-color: rgba(255, 0, 0, 0.1);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    /* max-width: 33%; */
-  }
-
-  .DetalleOrigenFamilia {
-    background-color: transparent;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    /* max-width: 33%; */
-  }
-
-  .DetalleOrigenVariantes {
-    background-color: transparent;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    /* max-width: 33%; */
-    /*border-left: 1px solid #000;*/
-    width: 100%;
-  }
- }
-
 </style>
 
 <header>
