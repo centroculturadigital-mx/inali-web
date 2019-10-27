9c8ee20dfdf356734fded346a1cca09604b211c5
MitziYolotzin <mitziulter@gmail.com>
2019-10-26 20:04:22 -0500

    ajuste mediqueries width comp Lateral

--- src/componentes/general/Lateral/Lateral.svelte
+++ src/componentes/general/Lateral/Lateral.svelte
@@ -15,6 +15,34 @@
         background-color: #fff;
         overflow-y: auto;
     }
+
+@media  (max-width: 575.98px) {
+    .Lateral {
+    width: 100vw;
+    max-width: 100%;
+    background-color: rgba(70,93,114, 1);
+    height: 100vh;
+    overflow-y: auto;
+    }
+}
+
+@media (min-width: 576px) and (max-width: 768px)  {
+    .Lateral {
+    width: 100vw;
+    max-width: 100%;
+    height: 100vh;
+    overflow-y: auto;
+    }
+}
+
+@media (min-width: 768px) and (max-width: 991.98px) {
+    .Lateral {
+    width: 100vw;
+    max-width: 100%;
+    height: 100vh;
+    overflow-y: auto;
+    }
+}
 </style>
 
 <aside class="Lateral" style={ `background-color: ${backgroundColor}`}>
