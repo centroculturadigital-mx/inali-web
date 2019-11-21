ce3e18c53eb7a6820bba569848883432ba596fd5
josecaos <jazzvoon@gmail.com>
2019-11-20 21:02:05 -0600

    Accion botones ventana resumen

--- src/componentes/general/LenguaResumen/LenguaResumen.svelte
+++ src/componentes/general/LenguaResumen/LenguaResumen.svelte
@@ -2,13 +2,18 @@
   import { onMount, createEventDispatcher } from "svelte";
   import { fade } from "svelte/transition";
 
-  // your script goes here
+  import AudiosContenedor from "../Galerias/AudiosContenedor.svelte";
+  import Fotos from "../Galerias/Fotos.svelte";
+  import Textiles from "../Galerias/Textiles.svelte";
+
   export let lengua;
+  
   let iconoCierra = "icono.cierra.circulo.svg";
   let iconoMinimiza = "minimiza.circulo.svg";
   let iconoAudios = "boton.play.solid.svg";
   let iconoTextiles = "boton.textiles.solid.svg";
   let iconoFotos = "boton.fotos.solid.svg";
+  
   const dispatch = createEventDispatcher();
 
   const cerrar = event => {
@@ -36,7 +41,20 @@
     return content;
   };
 
-  console.log("DEBUGGG:::", lengua.riesgo, riesgo);
+
+let ventanaGaleria = null;
+
+  const abreGaleria = (tipo) => {
+    ventanaGaleria = tipo;
+  };
+
+  const cerrarGaleria = event => {
+    ventanaGaleria = null;
+  };
+
+
+  // console.log("DEBUGGG:::", lengua.riesgo, riesgo);
+
 </script>
 
 <style>
@@ -366,7 +384,7 @@
       <!-- Botones Media -->
       <section class="BotonesMedia">
         <ul>
-          <li>
+          <li on:click={()=>{abreGaleria("audios")}}>
             <span class="BotonAudios">
               <div>
                 <img src={iconoAudios} alt="Audio" />
@@ -374,7 +392,7 @@
               <p>{lengua.audios.length ? lengua.audios.length : 0} Audios</p>
             </span>
           </li>
-          <li>
+          <li on:click={()=>{abreGaleria("textiles")}}>
             <span class="BotonTextiles">
               <div>
                 <img src={iconoTextiles} alt="Textiles" />
@@ -384,7 +402,7 @@
               </p>
             </span>
           </li>
-          <li>
+          <li on:click={()=>{abreGaleria("fotos")}}>
             <span class="BotonFotos">
               <div>
                 <img src={iconoFotos} alt="Fotos" />
@@ -407,4 +425,13 @@
       </div>
     </footer>
   </section>
+{/if}
+
+<!-- Galerias  -->
+{#if ventanaGaleria === 'audios'}
+  <AudiosContenedor audios={lengua.audios} on:click={cerrarGaleria} />
+{:else if ventanaGaleria === 'fotos'}
+  <Fotos imagenes={lengua.fotografias} on:click={cerrarGaleria} />
+{:else if ventanaGaleria === 'textiles'}
+  <Textiles imagenes={lengua.textiles} on:click={cerrarGaleria} />
 {/if}
