9c99c965811dcfef67b8f22278bb1bc9b2fd3049
ralex <ralex.rdz@gmail.com>
2020-02-24 22:27:35 -0600

    imagenes 448 o full

--- src/funciones/escalarImagenCloudinary.js
+++ src/funciones/escalarImagenCloudinary.js
@@ -1,4 +1,9 @@
-export default (src,w=192) => {
-  const partes = src.split("upload/")
-  return partes[0] + "upload/w_"+w+"/" + partes[1]
+export default (src,w=448) => {
+  let result = src
+  if (w < 600) {
+    const partes = src.split("upload/")
+    result = partes[0] + "upload/w_"+448+"/" + partes[1]
+
+  }
+  return result
 }
